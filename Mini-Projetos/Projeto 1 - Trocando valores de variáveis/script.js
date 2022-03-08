61aedc9a6fb79ee6ee7bcd94bff11c9d954f3339
Guilherme <guilhermeap.macedo20@gmail.com>
2022-03-08 19:55:48 -0300

    MiniProjeto - Alterando valores de duas variáveis

---
+++ Mini-Projetos/Projeto 1 - Trocando valores de variáveis/script.js
@@ -0,0 +1,7 @@
+var a = "Azul";
+var b = "Vermelho";
+console.log("Variável a está com: " + a + " e a variável b com: " + b)
+var aux = a;
+var a = b;
+var b= aux;
+console.log("Variável a ficou com: " + a + " e a variável b com: " + b)
--- Projetos/Projeto 1 - Trocando valores de variáveis/script.js
+++
@@ -1,2 +0,0 @@
-let teste = [1,2,3,4];
-console.log(teste)
