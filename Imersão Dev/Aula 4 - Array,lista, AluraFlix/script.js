1638a5b60aac9110a1fcaa33a98261a8b26e0a79
Guilherme <guilhermeap.macedo20@gmail.com>
2022-03-10 19:11:10 -0300

    AluraFlix - Plataforma de inserir imagens do javascript no HTML

--- Imersão Dev/Aula 4 - Array,lista, AluraFlix/script.js
+++ Imersão Dev/Aula 4 - Array,lista, AluraFlix/script.js
@@ -1,23 +1,24 @@
-var numSorteado = parseInt(Math.random()*11);
-var mensagem;
-var tentativas = 0;
-function Chutar(){
-    var numEscolhido = parseInt(document.getElementById("valor").value);
-    var base = document.getElementById("resultado");
-    if (tentativas>=3){
-        mensagem = "Máximos de tentativas atingidas, o número era: " + numSorteado;
-        } else if(numEscolhido === numSorteado){
-            mensagem = "Acertou!!!";
-        } else if((numEscolhido>10) || (numEscolhido<0)){
-            mensagem = "Por favor entre com um número de 0 a 10!";
-        }else{            
-            if (numEscolhido> numSorteado){
-                mensagem = "Tente um número menor <br>";
-            }else{
-                mensagem = "Tente um número maior <br>";
-            }
-            tentativas++;
-            mensagem += "Errou! Já tentou: " + tentativas;        
+var listaFilmes = [];
+
+function adicionarImagem(){
+    var foiAdicionada = false;
+    var result = document.getElementById("result");
+    var entrada = document.getElementById("entrada").value;
+    result.innerHTML ='';
+    listaFilmes.forEach(item => {
+        if(entrada == item){
+            foiAdicionada = true;
+        }
+    });    
+    if (foiAdicionada == true){
+        alert("Imagem já adicionada!")
+    }else{
+        listaFilmes.push(entrada);
     }
-    base.innerHTML = mensagem;
-};
+    mostraCapas();
+ }
+ function mostraCapas(){
+    for(var i=0;i<listaFilmes.length;i++){
+        result.innerHTML += "<img src=" + listaFilmes[i] + ">" 
+    }
+ }
