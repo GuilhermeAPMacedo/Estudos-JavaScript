db567d88aedde4e40ae1135ad67264fbfbdd85ff
Guilherme <guilhermeap.macedo20@gmail.com>
2022-03-10 19:30:05 -0300

    Correção aluraflix

--- Imersão Dev/Aula 4 - Array,lista, AluraFlix/script.js
+++ Imersão Dev/Aula 4 - Array,lista, AluraFlix/script.js
@@ -1,22 +1,17 @@
 var listaFilmes = [];
+var result = document.getElementById("result");
 
-function adicionarImagem(){
-    var foiAdicionada = false;
-    var result = document.getElementById("result");
+function adicionarImagem(){  
     var entrada = document.getElementById("entrada").value;
     result.innerHTML ='';
-    listaFilmes.forEach(item => {
-        if(entrada == item){
-            foiAdicionada = true;
-        }
-    });    
-    if (foiAdicionada == true){
+    if(listaFilmes.includes(entrada)){
         alert("Imagem já adicionada!")
     }else{
         listaFilmes.push(entrada);
     }
     mostraCapas();
  }
+
  function mostraCapas(){
     for(var i=0;i<listaFilmes.length;i++){
         result.innerHTML += "<img src=" + listaFilmes[i] + ">" 
